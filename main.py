c705718836429218d3fe5ef6ead24dbf3178faaf
Dan <danielsantosv8@usp.br>
2020-03-10 11:32:06 -0300

    BixosBot 1.0

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-#https://medium.com/@mdcg.dev/desenvolvendo-o-seu-primeiro-chatbot-no-telegram-com-python-a9ad787bdf6
+# https://medium.com/@mdcg.dev/desenvolvendo-o-seu-primeiro-chatbot-no-telegram-com-python-a9ad787bdf6
 
 import os
 from telegram import Bot, Update
 from telegram.ext import Dispatcher, CommandHandler
-from settings import USERS_TO_PING, TELEGRAM_TOKEN
 
 
-def all_callback(bot, update, **optional_args):
-    update.message.reply_text(os.environ[USERS_TO_PING], quote=False)
+def all_callback(bot, update: Update, **optional_args):
+    update.message.reply_text(os.environ['USERS_TO_PING'], quote=False)
 
 
-def web_hook(request):
-    bot = Bot(token=os.environ[TELEGRAM_TOKEN])
+def webHook(request):
+    bot = Bot(token=os.environ['TELEGRAM_TOKEN'])
     dispatcher = Dispatcher(bot, None, 0)
     dispatcher.add_handler(CommandHandler('all', all_callback))
 
@@ -19,6 +18,3 @@ def web_hook(request):
         update = Update.de_json(request.get_json(force=True), bot)
         dispatcher.process_update(update)
     return 'ok'
-
-
-
--- settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-USERS_TO_PING = os.getenv('USERS_TO_PING')
